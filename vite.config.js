04541cfb5bbb189e124c8d28613619407cc9dc2e
Gustavo <trabajoglm2021@gmail.com>
2024-10-28 23:40:41 -0400

    base actualizadav3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://glm1996.github.io/vue-mariposas/'
+  base: 'https://GLM1996.github.io/vue-mariposas/'
 })
